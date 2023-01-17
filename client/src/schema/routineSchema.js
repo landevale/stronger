c108892b2234d2a8ad1bd5bace4bb93892439720
Ambrose <ambroseheng@gmail.com>
2023-01-17 17:09:35 +0800

    Fixed Yup schema

--- client/src/schema/routineSchema.js
+++ client/src/schema/routineSchema.js
@@ -3,13 +3,13 @@ import * as Yup from "yup";
 export const routineSchema = Yup.object().shape({
   name: Yup.string().required("Is required"),
   user_id: Yup.string(),
-  exercises: [
-    {
-      name: { type: String, required: true },
+  exercises: Yup.array().of(
+    Yup.object().shape({
+      name: Yup.string().required(),
       sets: Yup.object().shape({
         reps: Yup.number().integer().positive(),
         weight: Yup.number().positive(),
       }),
-    },
-  ],
+    })
+  ),
 });
